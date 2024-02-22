64518961dca1feeb89ca064b2d5f9b174a10a1ca
Arjun kumar <arjun.k@zyapaar.com>
2024-02-22 11:45:48 +0530

    Fix : Initial commit

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+//secret file
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    APP_ID: "7083391708376854",
+    APP_SECRET: "886a676253fdfe0b594c080e988b8077",
+    MONGO_URI: "mongodb://localhost:27017/",
+    JWT_SECRET: "cvbugnuri0hvurt286356tchneugeugmawialfobfiughcm4",
+  },
+};
